05f325d21597dbfb9e863c24df7ba383d5cc5be2
Andrew-cmp <houhw49@gmail.com>
2024-05-04 17:38:04 +0800

    fix bug about generate_

---
+++ build/test/CMakeFiles/test_kuiper.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/xiaohou/Desktop/myinfer/test/test_data/test_load_data.cpp" "test/CMakeFiles/test_kuiper.dir/test_data/test_load_data.cpp.o" "gcc" "test/CMakeFiles/test_kuiper.dir/test_data/test_load_data.cpp.o.d"
+  "/home/xiaohou/Desktop/myinfer/test/test_data/test_tensor.cpp" "test/CMakeFiles/test_kuiper.dir/test_data/test_tensor.cpp.o" "gcc" "test/CMakeFiles/test_kuiper.dir/test_data/test_tensor.cpp.o.d"
+  "/home/xiaohou/Desktop/myinfer/test/test_layer/test_conv.cpp" "test/CMakeFiles/test_kuiper.dir/test_layer/test_conv.cpp.o" "gcc" "test/CMakeFiles/test_kuiper.dir/test_layer/test_conv.cpp.o.d"
+  "/home/xiaohou/Desktop/myinfer/test/test_layer/test_expression.cpp" "test/CMakeFiles/test_kuiper.dir/test_layer/test_expression.cpp.o" "gcc" "test/CMakeFiles/test_kuiper.dir/test_layer/test_expression.cpp.o.d"
+  "/home/xiaohou/Desktop/myinfer/test/test_layer/test_layer_factory.cpp" "test/CMakeFiles/test_kuiper.dir/test_layer/test_layer_factory.cpp.o" "gcc" "test/CMakeFiles/test_kuiper.dir/test_layer/test_layer_factory.cpp.o.d"
+  "/home/xiaohou/Desktop/myinfer/test/test_layer/test_maxpooling.cpp" "test/CMakeFiles/test_kuiper.dir/test_layer/test_maxpooling.cpp.o" "gcc" "test/CMakeFiles/test_kuiper.dir/test_layer/test_maxpooling.cpp.o.d"
+  "/home/xiaohou/Desktop/myinfer/test/test_layer/test_param_layer.cpp" "test/CMakeFiles/test_kuiper.dir/test_layer/test_param_layer.cpp.o" "gcc" "test/CMakeFiles/test_kuiper.dir/test_layer/test_param_layer.cpp.o.d"
+  "/home/xiaohou/Desktop/myinfer/test/test_layer/test_relu.cpp" "test/CMakeFiles/test_kuiper.dir/test_layer/test_relu.cpp.o" "gcc" "test/CMakeFiles/test_kuiper.dir/test_layer/test_relu.cpp.o.d"
+  "/home/xiaohou/Desktop/myinfer/test/test_main.cpp" "test/CMakeFiles/test_kuiper.dir/test_main.cpp.o" "gcc" "test/CMakeFiles/test_kuiper.dir/test_main.cpp.o.d"
+  "/home/xiaohou/Desktop/myinfer/test/test_runtime/test_attr.cpp" "test/CMakeFiles/test_kuiper.dir/test_runtime/test_attr.cpp.o" "gcc" "test/CMakeFiles/test_kuiper.dir/test_runtime/test_attr.cpp.o.d"
+  "/home/xiaohou/Desktop/myinfer/test/test_runtime/test_param.cpp" "test/CMakeFiles/test_kuiper.dir/test_runtime/test_param.cpp.o" "gcc" "test/CMakeFiles/test_kuiper.dir/test_runtime/test_param.cpp.o.d"
+  "/home/xiaohou/Desktop/myinfer/test/test_runtime/test_runtime_ir.cpp" "test/CMakeFiles/test_kuiper.dir/test_runtime/test_runtime_ir.cpp.o" "gcc" "test/CMakeFiles/test_kuiper.dir/test_runtime/test_runtime_ir.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/xiaohou/Desktop/myinfer/build/CMakeFiles/kuiper.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/test/CMakeFiles/test_kuiper.dir/cmake_clean.cmake
@@ -0,0 +1,33 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_kuiper.dir/test_data/test_load_data.cpp.o"
+  "CMakeFiles/test_kuiper.dir/test_data/test_load_data.cpp.o.d"
+  "CMakeFiles/test_kuiper.dir/test_data/test_tensor.cpp.o"
+  "CMakeFiles/test_kuiper.dir/test_data/test_tensor.cpp.o.d"
+  "CMakeFiles/test_kuiper.dir/test_layer/test_conv.cpp.o"
+  "CMakeFiles/test_kuiper.dir/test_layer/test_conv.cpp.o.d"
+  "CMakeFiles/test_kuiper.dir/test_layer/test_expression.cpp.o"
+  "CMakeFiles/test_kuiper.dir/test_layer/test_expression.cpp.o.d"
+  "CMakeFiles/test_kuiper.dir/test_layer/test_layer_factory.cpp.o"
+  "CMakeFiles/test_kuiper.dir/test_layer/test_layer_factory.cpp.o.d"
+  "CMakeFiles/test_kuiper.dir/test_layer/test_maxpooling.cpp.o"
+  "CMakeFiles/test_kuiper.dir/test_layer/test_maxpooling.cpp.o.d"
+  "CMakeFiles/test_kuiper.dir/test_layer/test_param_layer.cpp.o"
+  "CMakeFiles/test_kuiper.dir/test_layer/test_param_layer.cpp.o.d"
+  "CMakeFiles/test_kuiper.dir/test_layer/test_relu.cpp.o"
+  "CMakeFiles/test_kuiper.dir/test_layer/test_relu.cpp.o.d"
+  "CMakeFiles/test_kuiper.dir/test_main.cpp.o"
+  "CMakeFiles/test_kuiper.dir/test_main.cpp.o.d"
+  "CMakeFiles/test_kuiper.dir/test_runtime/test_attr.cpp.o"
+  "CMakeFiles/test_kuiper.dir/test_runtime/test_attr.cpp.o.d"
+  "CMakeFiles/test_kuiper.dir/test_runtime/test_param.cpp.o"
+  "CMakeFiles/test_kuiper.dir/test_runtime/test_param.cpp.o.d"
+  "CMakeFiles/test_kuiper.dir/test_runtime/test_runtime_ir.cpp.o"
+  "CMakeFiles/test_kuiper.dir/test_runtime/test_runtime_ir.cpp.o.d"
+  "test_kuiper"
+  "test_kuiper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_kuiper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
